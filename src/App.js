c99bb52fb7c1dcf8b9bc774df6f9460123d56dfb
kiran <kirandeepstar123@gmail.com>
2020-12-27 02:20:12 -0500

    tests

--- src/App.js
+++ src/App.js
@@ -3,15 +3,17 @@ import AutoCompletedText from './AutoCompletedText'
 import './App.css';
 
 class App extends React.Component{
-  constructor(props){
-    super(props);
-  }
+  // constructor(props){
+  //   super(props);
+  // }
   
 
 
   render(){
     return(
-      <AutoCompletedText />
+      <div data-test="component-app">
+        <p>kiran</p>
+      </div>
     )
   }
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,33 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import Enzyme,{shallow} from "enzyme"
+import EnzymeAdapter from "enzyme-adapter-react-16"
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+Enzyme.configure({adapter  : new EnzymeAdapter()})
+
+test('renders without error', () => {
+   const wrapper = shallow (<App />)
+   const appComponent = wrapper.find("[data-test='component-app']")
+   expect(appComponent.length).toBe(1)
+});
+
+
+test('renders button', () => {
+  
+});
+
+
+test('renders counter display', () => {
+  
+});
+
+
+test('counter starts at 0', () => {
+  
+});
+
+
+test('clicking on button increments number', () => {
+  
 });
